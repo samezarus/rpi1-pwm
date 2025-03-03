fab798d3349c65b61b21639f2a4c0d5aecc66ab8
samezarus <samezarus@gmail.com>
2025-03-04 08:53:09 +1000

    upd

---
+++ main-flask.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = "Adam"  
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
