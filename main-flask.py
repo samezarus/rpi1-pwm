b83aa53be332ada4ffc0de0e326a25e7d292b0ac
samezarus <samezarus@gmail.com>
2025-03-04 19:56:51 +1000

    upd

--- main-flask.py
+++ main-flask.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     name = "Adam"  
-    return render_template('index.html', name=name)
+    return render_template('./templates/index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
